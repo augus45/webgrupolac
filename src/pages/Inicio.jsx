79a86ae192a1a242b658dbafa2bf297492956450
augus45 <augustoacua2011@gmail.com>
2026-05-08 17:24:07 -0300

    Agrega banner Distribuidora Mayorista antes de Acceso Clientes

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -9,6 +9,13 @@ export default function Inicio() {
       <Hero />
       <Novedades />
       <Banners />
+      <section className="banners">
+        <div className="banners__item">
+          <a href="https://www.lac.com.ar/website/?p=643" target="_blank" rel="noopener noreferrer">
+            <img src="https://sp-ao.shortpixel.ai/client/to_webp,q_glossy,ret_img/https://www.lac.com.ar/website/wp-content/uploads/2015/07/lac_dh_DM_exigenciamercado.png" alt="LAC Distribuidora Mayorista de Combustibles y Lubricantes" loading="lazy" />
+          </a>
+        </div>
+      </section>
       <Clientes />
     </>
   )
